0dad69d5cfb284241a27fc3ed8f172e0c6976850
Mark Lester Morta <marklester10.mlsm@gmail.com>
2020-07-14 17:36:20 +0800

    variables and console

--- Console and Variables/app.js
+++ Console and Variables/app.js
@@ -1,21 +1,21 @@
 //Log console
-// console.log("Hello Pare!");
-// console.log(123);
-// console.log(true);
-// var greeting = "Hello";
-// console.log(greeting);
-// console.log([1, 2, 3, 4, 5]);
-// console.log({ a: "Hello", b: "Hi" });
-// console.table({ a: "Hello", b: "Hi" });
-// console.error("Awit may mali!");
-// console.warn("Malapit ka na mag ka error.");
-// console.clear();
+console.log("Hello Pare!");
+console.log(123);
+console.log(true);
+var greeting = "Hello";
+console.log(greeting);
+console.log([1, 2, 3, 4, 5]);
+console.log({ a: "Hello", b: "Hi" });
+console.table({ a: "Hello", b: "Hi" });
+console.error("Awit may mali!");
+console.warn("Malapit ka na mag ka error.");
+console.clear();
 
-// console.time("Time");
-// console.log("Hello Pare!");
-// console.log("Malapit ka na magtime");
-// console.log("Malapit ka na magtime");
-// console.timeEnd("Time");
+console.time("Time");
+console.log("Hello Pare!");
+console.log("Malapit ka na magtime");
+console.log("Malapit ka na magtime");
+console.timeEnd("Time");
 
 //VARIABLES
 
