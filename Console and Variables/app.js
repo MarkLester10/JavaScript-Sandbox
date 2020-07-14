88c37eb911a6b8592a16186cede916f5835b26ff
Mark Lester Morta <marklester10.mlsm@gmail.com>
2020-07-14 16:29:02 +0800

    console and variables

---
+++ Console and Variables/app.js
@@ -0,0 +1,32 @@
+//Log console
+// console.log("Hello Pare!");
+// console.log(123);
+// console.log(true);
+// var greeting = "Hello";
+// console.log(greeting);
+// console.log([1, 2, 3, 4, 5]);
+// console.log({ a: "Hello", b: "Hi" });
+// console.table({ a: "Hello", b: "Hi" });
+// console.error("Awit may mali!");
+// console.warn("Malapit ka na mag ka error.");
+// console.clear();
+
+// console.time("Time");
+// console.log("Hello Pare!");
+// console.log("Malapit ka na magtime");
+// console.log("Malapit ka na magtime");
+// console.timeEnd("Time");
+
+//VARIABLES
+
+//var, let ,const
+
+var name = "John Doe";
+console.log(name);
+name = "Jane Doe";
+console.log(name);
+
+//Initializing Variable
+var greeting;
+greeting = "Hello There";
+console.log(greeting);
