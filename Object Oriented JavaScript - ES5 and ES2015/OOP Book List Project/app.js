51e96298e8f74b2e268d16006aae5e56247b9663
Mark Lester Morta <marklester10.mlsm@gmail.com>
2020-07-27 18:01:08 +0800

    OOP Book List App using E2015

---
+++ Object Oriented JavaScript - ES5 and ES2015/OOP Book List Project/app.js
@@ -0,0 +1,106 @@
+//ES2015 or ES5
+
+//globals
+spinner = document.getElementById("spinner");
+button = document.getElementById("submit");
+
+//Book Cosntructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+//UI Constructor -set of prototype methods
+function UI() {}
+
+UI.prototype.addBookToList = function (book) {
+  spinner.className = "d-block";
+  button.className = "btn btn-block btn-dark d-none";
+  setTimeout(() => {
+    const list = document.getElementById("book-list");
+    //Create tr element
+    const row = document.createElement("tr");
+    //Insert td's
+    row.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.isbn}</td>
+  <td><a href="#" class="delete"><i class="fa fa-trash text-danger cursor-pointer"></i></a></td>`;
+
+    //append to list
+    list.appendChild(row);
+
+    //remove spinner
+    spinner.className = "d-none";
+    button.className = "btn btn-block btn-dark d-block";
+
+    //show success alert
+    this.showAlert(
+      "New Book Added Successfully",
+      "alert alert-success d-block"
+    );
+  }, 2000);
+};
+
+//Delete Book
+UI.prototype.deleteBook = function (target) {
+  if (target.className === "delete") {
+    target.parentElement.parentElement.remove();
+  }
+};
+
+//Clear Fields
+UI.prototype.clearFields = function () {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+};
+
+//showAlert
+UI.prototype.showAlert = function (message, type) {
+  alert = document.querySelector(".alert");
+
+  alert.className = type;
+  alert.textContent = message;
+
+  //timeout after 3s
+  setTimeout(function () {
+    alert.className = "alert alert-success d-none";
+  }, 3000);
+};
+
+//Event Listener for add
+document.getElementById("book-form").addEventListener("submit", function (e) {
+  //Get form values
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
+
+  //Instantiating a book
+  const book = new Book(title, author, isbn);
+
+  //Instantiate UI
+  const ui = new UI();
+
+  //Validate
+  if (title === "" || author === "" || isbn === "") {
+    ui.showAlert("Please fill in all fields", "alert alert-danger d-block");
+  } else {
+    //Add book to list
+    ui.addBookToList(book);
+
+    //Clear fiels
+    ui.clearFields();
+  }
+  e.preventDefault();
+});
+
+//Event Listener for delete
+document.getElementById("book-list").addEventListener("click", function (e) {
+  const ui = new UI();
+  ui.deleteBook(e.target.parentElement);
+  //show alert
+  ui.showAlert("Book Deleted Successfully", "alert alert-success d-block");
+  e.preventDefault();
+});
